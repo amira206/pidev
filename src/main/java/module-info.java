e82684388239a85b49a29e2dd2f693f4592573b6
yassineamri1212 <amri.yassine.1@esprit.tn>
2025-02-19 20:54:46 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module com.example.gestionevenement3a16 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires jdk.jsobject;
+    requires java.desktop;
+    requires javafx.graphics;
+    requires com.google.gson;
+
+    opens GestionEvenement3a16.Entity to com.google.gson;
+
+    opens GestionEvenement3a16 to javafx.fxml;
+    exports GestionEvenement3a16;
+    exports connectionSql;
+    opens connectionSql to javafx.fxml;
+    exports GestionEvenement3a16.Controller;
+    opens GestionEvenement3a16.Controller to javafx.fxml;
+    exports GestionEvenement3a16.Entity;
+  //  exports GestionEvenement3a16.Services;
+    opens GestionEvenement3a16.Services to com.google.gson;
+}
+
