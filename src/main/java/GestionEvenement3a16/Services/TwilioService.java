3e737b80e3bf39eff64ea8ed55407adcb64cccbf
yassineamri1212 <amri.yassine.1@esprit.tn>
2025-03-06 00:44:58 +0100

    gestion evenement final2

--- src/main/java/GestionEvenement3a16/Services/TwilioService.java
+++ src/main/java/GestionEvenement3a16/Services/TwilioService.java
@@ -9,8 +9,8 @@ public class TwilioService {
     //private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     // private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     // These are your Twilio Account SID and Auth Token
-    private static final String ACCOUNT_SID = "AC52149aef69c9bd900726b24259acad9b";
-    private static final String AUTH_TOKEN = "69998e285c3877f52cab6a5aa8e76ca2";
+    private static final String ACCOUNT_SID = "";
+    private static final String AUTH_TOKEN = "";
     public void sendSms(String to, String from, String body) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
